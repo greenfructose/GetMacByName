a93036f29936e5cb162eb5d129dbd1266c30276a
greenfructose <justinclarkturney@gmail.com>
2022-01-28 16:34:27 -0600

    Refactored into different functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import sys
+
 from manage_switches.DoConcurrent import *
 from manage_switches.DocFunctions import *
 from manage_switches.NetworkFunctions import *
@@ -10,8 +12,7 @@ from secret import IP_RANGE, USERNAME, PASSWORD, DEVICE_TYPE
 
 def write_arp_tables(ip: str):
     """
-    Logs into switch, pings every address in IP_RANGE, then gets the
-    ARP table from each switch. Writes remote device IP, remote device
+    Retrieves ARP table from switch. Writes remote device IP, remote device
     MAC, remote device hostname, switch ip, and switch port the remote
     device is connected too into a CSV file.
     :param ip: IP address of switch as string
@@ -25,20 +26,21 @@ def write_arp_tables(ip: str):
             'username': USERNAME,
             'password': PASSWORD
         }
-        commands = ['arp']
-        ip_list = generate_ip_list(IP_RANGE)
-        ping_from_switch(coninfo, ip_list)
+        commands = ['show arp']
         run_commands(coninfo, commands)
         arp_list = []
         spinner.start(f'\nGetting ARP table from switch at {ip}')
-        with open(f'switch_arp/{ip}', 'r') as f:
+        with open(f'switch_show_arp/{ip}', 'r') as f:
             raw_arp_table = f.read()
         spinner.succeed()
         spinner.stop()
         spinner.start(f'\nFormatting ARP table and writing to file.')
         fixed_arp_list = [x.strip() for x in raw_arp_table.split("\n")[6:-2]]
         for item in fixed_arp_list:
-            item = item.replace('     ', ' ').replace('    ', ' ').replace('   ', ' ').replace('  ', ' ')
+            item = item.replace('     ', ' ')\
+                .replace('    ', ' ')\
+                .replace('   ', ' ')\
+                .replace('  ', ' ')
             item = item.split(' ')
             if len(item) > 3:
                 arp_list.append({
@@ -49,16 +51,48 @@ def write_arp_tables(ip: str):
                     'Switch Port': item[3]
                 })
         write_result_csv(arp_list, 'a+', prepend=ip)
-        spinner.succeed(f'\nFile written to {ip}-srp_list.csv')
+        spinner.succeed(f'\nFile written to {ip}-arp_list.csv')
         spinner.stop()
         return f'Success on {ip}'
     except (KeyboardInterrupt, SystemExit):
         spinner.stop()
 
 
+def populate_arp_table(ip: str):
+    """
+    Logs into switch, pings every address in IP_RANGE to populate ARP table
+    :param ip: IP address of switch as string
+    :return: Success string 'Success on {ip}'
+    """
+    try:
+        coninfo = {
+            'ip': ip,
+            'device_type': DEVICE_TYPE,
+            'username': USERNAME,
+            'password': PASSWORD
+        }
+        ip_list = generate_ip_list(IP_RANGE)
+        ping_from_switch(coninfo, ip_list)
+        return f'Success on {ip}'
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
+
+
 if __name__ == '__main__':
+    coninfo = {
+        'ip': '',
+        'device_type': DEVICE_TYPE,
+        'username': USERNAME,
+        'password': PASSWORD
+    }
     switch_ips = []
+    ping_ips = []
+    ip_list = generate_ip_list(IP_RANGE)
+    with open('PingSwitch.csv', 'r') as f:
+        for row in csv.reader(f):
+            ping_ips.append(row[0])
     with open('SwitchAddresses.csv', 'r') as f:
         for row in csv.reader(f):
             switch_ips.append(row[0])
-    multithread(write_arp_tables, switch_ips)
+    multithread(populate_arp_table, ['192.168.2.9'])
+    multithread(write_arp_tables, ['192.168.2.9'])
